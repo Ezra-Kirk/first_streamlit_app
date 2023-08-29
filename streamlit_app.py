f6f7e8621ed0f8bd5840ead9eb33f8d259963ba4
Ezra-Kirk <60789552+Ezra-Kirk@users.noreply.github.com>
2023-08-29 14:00:23 +0100

    Update streamlit_app.py
    bracket typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ streamlit.header("Breakfast Menu")
 streamlit.text("🥣 Omega and Blueberry Oats")
 streamlit.text("🥗 Kale & Spinach Smoothie")
 streamlit.text("🐔 Hard-boiled Egg")
-streamlit.text("🥑🍞 Avocado Toast"
+streamlit.text("🥑🍞 Avocado Toast")
