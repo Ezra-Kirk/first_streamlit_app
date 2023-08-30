ec9a28949e79c2aec8a7cbaeebda4ce9998f1040
Ezra-Kirk <60789552+Ezra-Kirk@users.noreply.github.com>
2023-08-30 10:23:49 +0100

    Update streamlit_app.py
    json response to pandas df

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 # New section to import Fruity Vice response
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# turn json response into tabular data
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# then turn into dataframe
+streamlit.dataframe(fruityvice_normalized)
