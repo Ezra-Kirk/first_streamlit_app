e8fdea08652960ada2638f753ef93e2e5f1437be
Ezra-Kirk <60789552+Ezra-Kirk@users.noreply.github.com>
2023-08-30 11:37:44 +0100

    Update streamlit_app.py
    connecting to Snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,13 @@ streamlit.write('The user entered ', fruit_choice)
 # New section to import Fruity Vice response
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 
 # turn json response into tabular data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
